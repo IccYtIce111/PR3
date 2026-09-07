c35cf8c393ed209adc5bb91a6a7d2bedbcb49e0f
SAD.GEROIN <iccyice66@gmail.com>
2026-09-07 15:41:38 +0700

    Create footer.php

---
+++ config/includes/templates/templates/footer.php
@@ -0,0 +1,10 @@
+    </div>
+    <footer class="footer mt-5 py-3 bg-light">
+        <div class="container text-center">
+            <span class="text-muted">© 2024 Гостиница «Сладкие сны». Все права защищены.</span>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="public/js/script.js"></script>
+</body>
+</html>
